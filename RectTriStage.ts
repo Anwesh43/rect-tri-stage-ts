2f752e7571a37cab76aff29b5a869ef2eba0ae49
Anwesh43 <anweshthecool0@gmail.com>
2019-04-14 22:04:18 +0530

    adding RTNode class

--- RectTriStage.ts
+++ RectTriStage.ts
@@ -144,3 +144,49 @@ class Animator {
         }
     }
 }
+
+class RTNode {
+
+    prev : RTNode
+    next : RTNode
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new RTNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawRTNode(context, this.i, this.state.scale)
+        if (this.prev) {
+            this.prev.draw(context)
+        }
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : RTNode {
+        var curr : RTNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+
+}
